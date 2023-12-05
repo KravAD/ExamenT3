67911ed1e305003b5f4aebd983d0f525bed8c133
KravAD <dkrav@myuax.com>
2023-12-05 11:12:27 +0100

    commit 20
    Pruebo uso de using namespace Enviorment.

--- Enviorment/Enviorment.h
+++ Enviorment/Enviorment.h
@@ -6,10 +6,11 @@
 #include <string>
 #include <variant>
 using namespace std;
+using namespace Enviorment;
 class Enviorment {
 public:
     using symbol = variant<int, float, string>;
-    void insert(const string& symbol, const Enviorment::symbol& value);
+    void insert(const string& symbol, const symbol& value);
     symbol lookup(const string& symbol) const;
 
 private:
