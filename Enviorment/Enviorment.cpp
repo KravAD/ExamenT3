7c9795d6657bcdfc13e7b40ccec252c33b56da9d
KravAD <dkrav@myuax.com>
2023-12-05 11:15:20 +0100

    commit 22
    Implemento using namespace std en archivo Enviorment.cpp

--- Enviorment/Enviorment.cpp
+++ Enviorment/Enviorment.cpp
@@ -1,16 +1,18 @@
 #include "Enviorment.h"
 #include <stdexcept>
+using namespace std;
 
-void Enviorment::insert(const std::string& symbol, const Enviorment::symbol& value) {
+void Enviorment::insert(const string& symbol, const Enviorment::symbol& value) {
     if (symbols_.find(symbol) != symbols_.end()) {
-        throw std::runtime_error("Symbol already exists");
+        throw runtime_error("Symbol already exists");
     }
     symbols_[symbol] = value;
 }
 
-Enviorment::symbol Enviorment::lookup(const std::string& symbol) const{
+Enviorment::symbol Enviorment::lookup(const string& symbol) const{
     if (symbols_.find(symbol) == symbols_.end()) {
-        throw std::runtime_error("Symbol does not exist");
+        throw runtime_error("Symbol does not exist");
     }
     return symbols_.at(symbol);
 }
+
